0b95acc1592cfce7422f1b577ef6eea3b75e6751
patrickthompsonreis <patrickt.reis@gmail.com>
2022-06-14 20:16:51 -0300

    refactor:  comentando lógica não utilizada

---
+++ wg-script.js
@@ -0,0 +1,19 @@
+// const quotes = require('./quotes.json');
+
+// import * as quotes from './quotes.json';
+
+// function getQuote() {
+//     let randomId = Math.floor(Math.random() * quotes.length);
+//     console.log(randomId)
+//     document.getElementById('advice-quote').innerHTML = "<p>quotes[randomId].quote</p>";
+//     document.getElementById('advice-author').innerHTML = "<p>quotes[randomId].author</p>";
+// }
+
+// document.getElementById("advice-button").addEventListener('click', () => {
+//     let randomId = Math.floor(Math.random() * quotes.length);
+//     document.getElementById('advice-quote').innerHTML = quotes[randomId].quote;
+//     document.getElementById('advice-author').innerHTML = quotes[randomId].author;
+
+//     console.log(quotes[randomId].quote)
+//     console.log(quotes[randomId].author)
+// })
